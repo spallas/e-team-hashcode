88ee3e73a4499497aaecdd72dc9bce58b3e572a4
Alessandro Spadoni <alessandro.spadoni@enel.com>
2021-02-19 19:23:17 +0100

    naive cpp solution

--- pizza_cpp/main.cpp
+++ pizza_cpp/main.cpp
@@ -1,6 +1,8 @@
 #include<vector>
 #include<string>
 #include<iostream>
+#include <algorithm>
+#include <numeric>
 
 #include "FileManager.hpp"
 
@@ -9,7 +11,41 @@ void processFile(const std::string& fileName) {
     FileManager::OutputFile out;
 
     // solve
-    out.i = input.i + 1;
+
+	// sort pizzas by num ingredients
+	std::sort(input.pizzas.begin(), input.pizzas.end(), [](std::vector<std::string> ingrA, std::vector<std::string> ingrB){
+		return ingrA.size() > ingrB.size();
+	});
+
+	std::vector<bool> usedPizzas = std::vector<bool>(input.pizzas.size(), false);
+	int pizzasLeft = usedPizzas.size();
+
+	std::vector<int> teamSizes = {2, 3, 4};
+	for (auto tSize: teamSizes) {
+		for (int unused = 0; unused < input.teams[tSize - 2]; unused++) {
+			if (pizzasLeft < tSize)
+				break;
+
+			Delivery d;
+			d.teamSize = tSize;
+
+			for (int pizzaIdx = 0; pizzaIdx < usedPizzas.size(); pizzaIdx++) {
+				if (usedPizzas[pizzaIdx])
+					continue;
+
+				d.pizzas.push_back(pizzaIdx);
+				usedPizzas[pizzaIdx] = true;
+				pizzasLeft--;
+				if (d.pizzas.size() == d.teamSize)
+					break;
+			}
+
+			out.deliveries.push_back(d);
+		}
+	}
+
+	out.numDeliveries = out.deliveries.size();
+
 
     std::cout << FileManager::writeOutput(out, "./outputs/" + fileName + ".out") << std::endl;
 }
@@ -17,7 +53,7 @@ void processFile(const std::string& fileName) {
 
 int main()
 {
-	std::vector<std::string> files = { "prova" };
+	std::vector<std::string> files = { "a_example", "b_little_bit_of_everything", "c_many_ingredients", "d_many_pizzas", "e_many_teams" };
 	for (auto fName : files) {
 		std::cout << "Begin " << fName << std::endl;
 		processFile(fName);
