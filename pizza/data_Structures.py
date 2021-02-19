82879cffc4253e15f70fb9ee8971d7228e8539a0
Manzini Andrea (DS&G) <andrea.manzini@enel.com>
2021-02-19 18:40:30 +0100

    update with block matrices

--- pizza/data_Structures.py
+++ pizza/data_Structures.py
@@ -1,15 +1,18 @@
 input_file = 'b_little_bit_of_everything.in'
 
-def data_structures(input_file):
+from tqdm import tqdm
 
+def data_structures(input_file):
     pizzas_descr = {}
 
-    with open('b_little_bit_of_everything.in') as fin:
+    with open(input_file) as fin:
         lines = fin.readlines()
 
-        lines = list(map(lambda x:x.split(), lines))
+        lines = list(map(lambda x: x.split(), lines))
 
         n_pizzas = int(lines[0][0])
+
+        print(n_pizzas)
         nt2 = int(lines[0][1])
         nt3 = int(lines[0][2])
         nt4 = int(lines[0][3])
@@ -17,21 +20,19 @@ def data_structures(input_file):
         for idx, line in enumerate(lines[1:]):
             pizzas_descr[idx] = set(line[1:])
 
-    n_pizzas
-
-    pizzas_descr
-
     ingredients = set().union(*pizzas_descr.values())
 
     n_ingredients = len(ingredients)
-
 
     import numpy as np
     overlaps = np.zeros([n_pizzas, n_pizzas], dtype=int)
 
     from itertools import product
 
-    for i, j in product(range(n_pizzas), range(n_pizzas)):
-        overlaps[i][j] = len(pizzas_descr[i].intersection(pizzas_descr[j]))
+    for block in tqdm(range(int(n_pizzas / 100))):
 
-    return dict(overlaps=overlaps, pizzas_descr=pizzas_descr,ingredients=ingredients)
+        offset = block * 100
+        for i, j in product(range(100), range(100)):
+            overlaps[offset + i][offset + j] = len(pizzas_descr[offset + i].intersection(pizzas_descr[offset + j]))
+
+    return dict(overlaps=overlaps, pizzas_descr=pizzas_descr, ingredients=ingredients)
