19e74c0c886a0a03de32fd5d552b030f7b14bc58
Alessandro Spadoni <alessandro.spadoni@enel.com>
2021-02-19 18:49:06 +0100

    add cpp cloning script

---
+++ create_problem_folder_cpp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+name=$1
+if [ -z "$name" ]
+then
+  echo "name of project is empty"
+else
+  mkdir "$name"_cpp
+  mkdir "$name"_cpp/inputs
+  mkdir "$name"_cpp/outputs
+  cp hash_code_lib_cpp/FileManager.hpp "$name"_cpp/
+  cp hash_code_lib_cpp/main.cpp "$name"_cpp/
+  cp hash_code_lib_cpp/build.sh "$name"_cpp/
+  cp hash_code_lib_cpp/clean.sh "$name"_cpp/
+fi
