25a732e17a537a0f2046cd8d2d1e23711eb4f1c1
Spallaccini Davide (DS&G) <davide.spallaccini@enel.com>
2021-02-25 20:01:15 +0100

    Update

---
+++ traffic/parse.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Dict
+
+
+@dataclass
+class Street:
+    name: str
+    a: int
+    b: int
+    duration: int
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+@dataclass
+class Intersection:
+    id_: int
+    in_streets: List[Street]
+    out_streets: List[Street]
+
+    def __hash__(self):
+        return hash(self.id_)
+
+
+@dataclass
+class Car:
+    id_: int
+    path: List[Street]
+
+    def __hash__(self):
+        return hash(self.id_)
+
+
+def parse(file_path):
+    with Path(file_path).open('r') as f:
+        duration, intersections, streets, cars, points = f.readline().strip().split(' ')
+        duration, intersections, streets, cars, points = map(int, [duration, intersections, streets, cars, points])
+        street_map: Dict[str, Street] = {}
+        inters_map: Dict[int, Intersection] = {}
+        car_map: Dict[int, Car] = {}
+
+        for line in f:
+            if streets > 0:
+                streets -= 1
+                a, b, name, duration = line.strip().split(' ')
+                s = Street(name, a, b, duration)
+                street_map[name] = s
+                if a in inters_map:
+                    inters_map[a].in_streets.append(s)
+                else:
+                    inters_map[a] = Intersection(a, in_streets=[s], out_streets=[])
+                if b in inters_map:
+                    inters_map[b].out_streets.append(s)
+                else:
+                    inters_map[b] = Intersection(b, in_streets=[], out_streets=[s])
+            else:
+                cars -= 1
+                path_len, path = line.strip().split(' ', maxsplit=1)
+                path = path.split(' ')
+                path_ = []
+                for name in path:
+                    path_.append(street_map[name])
+                car_map[cars] = Car(cars, path_)
+
+        return street_map, inters_map, car_map
