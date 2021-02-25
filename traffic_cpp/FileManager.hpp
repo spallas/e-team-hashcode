d481aa34673116945352949771523459a0f598c4
Alessandro Spadoni <alessandro.spadoni@enel.com>
2021-02-25 20:35:31 +0100

    base solution

--- traffic_cpp/FileManager.hpp
+++ traffic_cpp/FileManager.hpp
@@ -7,6 +7,8 @@
 #include <sstream>
 #include <iterator>
 
+#include <map>
+
 std::vector<std::string> split(const std::string text) {
 	std::istringstream iss(text);
 	std::vector<std::string> results((std::istream_iterator<std::string>(iss)),
@@ -15,16 +17,101 @@ std::vector<std::string> split(const std::string text) {
 	return results;
 }
 
+std::string join(const std::vector<std::string> array) {
+	std::stringstream ss;
+	for (auto item : array)
+		ss << item << ", ";
+	return ss.str();
+}
+
+int to_int(const std::string& text) {
+	return std::atoi(text.c_str());
+}
+
+struct Street {
+	int intersectionStart;
+	int intersectionEnd;
+	std::string streetName;
+	int time;
+};
+
+struct Car {
+	int pathLength;
+	std::vector<std::string> path;
+};
+
+
+struct StreetSchedule {
+	std::string streetName;
+	int greenDuration;
+};
+
+struct Schedule {
+	int intersectionId;
+	int streetsControlled;
+	std::vector<StreetSchedule> schedules;
+};
+
 namespace FileManager {
 
 	struct InputFile {
         // define custom structure here
-        int i;
+		int Duration;
+		int Intersections;
+		int Streets;
+		int Cars;
+		int points;
+
+		std::vector<Street> streets;
+		std::vector<Car> cars;
+
+		std::map<std::string, int> streetsUsed;
+		std::map<int, std::vector<std::string>> intersectionsStreet;
+
+		std::string toString() {
+			std::stringstream ss;
+			ss << "Duration:" << this->Duration << std::endl;
+			ss << "Intersections:" << this->Intersections << std::endl;
+			ss << "Streets:" << this->Streets << std::endl;
+			ss << "Cars:" << this->Cars << std::endl;
+			ss << "points:" << this->points << std::endl;
+
+			ss << "streets: " << std::endl;
+			for (auto s : this->streets) {
+				ss << "\t" << "intersectionStart: " << s.intersectionStart << std::endl;
+				ss << "\t" << "intersectionEnd: " << s.intersectionEnd << std::endl;
+				ss << "\t" << "streetName: " << s.streetName << std::endl;
+				ss << "\t" << "time: " << s.time << std::endl << std::endl;
+			}
+
+			ss << "cars: " << std::endl;
+			for (auto c: this->cars) {
+				ss << "\t" << "pathLength: " << c.pathLength << std::endl;
+				ss << "\t" << "path: " << std::endl;
+				for (auto piece : c.path) {
+					ss << "\t\t" << piece << std::endl;
+				}
+				ss << std::endl;
+			}
+
+			ss << "streetsUsed: " << std::endl;
+			for (auto it = this->streetsUsed.begin(); it != this->streetsUsed.end(); it++) {
+				ss << "\t" << it->first << ", " << it->second << std::endl;
+			}
+
+			ss << "intersectionStreets: " << std::endl;
+			for (auto inters : this->intersectionsStreet) {
+				ss << "\t" << inters.first << ", " << join(inters.second) << std::endl;
+			}
+
+			return ss.str();
+		}
 	};
 
 	struct OutputFile {
 		// define custom structure here
-        int i;
+        int numSchedules;
+		std::vector<Schedule> schedules;
 	};
 
 	InputFile readInput(std::string path)
@@ -46,7 +133,42 @@ namespace FileManager {
 		auto fields = split(lines[0]);
 
         // parse lines
-        ret.i = std::atoi(fields[0].c_str());
+		ret.Duration = to_int(fields[0]);
+		ret.Intersections = to_int(fields[1]);
+		ret.Streets = to_int(fields[2]);
+		ret.Cars = to_int(fields[3]);
+		ret.points = to_int(fields[4]);
+
+		for (int i = 0; i < ret.Streets; i++) {
+			auto fields = split(lines[i+1]);
+			Street s;
+			s.intersectionStart = to_int(fields[0]);
+			s.intersectionEnd = to_int(fields[1]);
+			s.streetName = fields[2];
+			s.time = to_int(fields[3]);
+
+			ret.streets.push_back(s);
+
+			ret.streetsUsed[s.streetName] = 0;
+
+			if (ret.intersectionsStreet.count(s.intersectionEnd) == 0)
+				ret.intersectionsStreet[s.intersectionEnd] = std::vector<std::string>();
+
+			ret.intersectionsStreet[s.intersectionEnd].push_back(s.streetName);
+		}
+
+		for (int i = 0; i < ret.Cars; i++) {
+			auto fields = split(lines[i + 1 + ret.Streets]);
+			Car c;
+			c.pathLength = to_int(fields[0]);
+			for (int j = 0; j < c.pathLength; j++) {
+				auto streetName = fields[j+1];
+				c.path.push_back(streetName);
+
+				ret.streetsUsed[streetName]++;
+			}
+			ret.cars.push_back(c);
+		}
 
 		file.close();
 
@@ -63,7 +185,15 @@ namespace FileManager {
 		}
 
         // write output
-        outFile << out.i << std::endl;
+		outFile << out.numSchedules << std::endl;
+		for (auto sched : out.schedules) {
+			outFile << sched.intersectionId << std::endl;
+			outFile << sched.streetsControlled << std::endl;
+			for (auto streetSched: sched.schedules) {
+				outFile << streetSched.streetName << " " << streetSched.greenDuration << std::endl;
+			}
+		}
+
 
 		outFile.close();
 
--- traffic_cpp/main.cpp
+++ traffic_cpp/main.cpp
@@ -1,15 +1,69 @@
 #include<vector>
 #include<string>
 #include<iostream>
-
+#include <map>
+#include <cmath>
 #include "FileManager.hpp"
 
 void processFile(const std::string& fileName) {
 	auto input = FileManager::readInput("./inputs/" + fileName + ".in");
     FileManager::OutputFile out;
 
+	std::map<std::string, float> timePerFile = { {"a", 2}, {"b", 50}, {"c", 50}, {"d", 50}, {"e", 50}, {"f", 50} };
+
     // solve
-    out.i = input.i + 1;
+	const int FACTOR = 1;
+
+	for (auto inters : input.intersectionsStreet) {
+		auto iId = inters.first;
+		auto streets = inters.second;
+
+		Schedule s;
+		s.intersectionId = iId;
+		s.streetsControlled = streets.size();
+
+		// intersections with only one street --> always green
+		if (streets.size() == 1) {
+			StreetSchedule strS;
+			strS.streetName = streets[0];
+			strS.greenDuration = input.Duration == 1 ? 1 : 2;
+			s.schedules.push_back(strS);
+		} else {
+			// > 1 streets
+			auto lightDuration = std::vector<long>(streets.size());
+			long maxLighDuration = -1;
+			for (auto i = 0; i < streets.size(); i++) {
+				lightDuration[i] = input.streetsUsed[streets[i]] * FACTOR;
+				if (lightDuration[i] > maxLighDuration) {
+					maxLighDuration = lightDuration[i];
+				}
+			}
+
+			if (maxLighDuration == 0) {
+				continue;
+			}
+
+			double maxTime = (double)timePerFile[fileName];
+
+			for (auto i = 0; i < streets.size(); i++) {
+				double val = std::ceil(((double)(lightDuration[i]) / (double)(maxLighDuration) * maxTime));
+				if (val < 1.0e-3 ) {
+					val = 1;
+				}
+				lightDuration[i] = (long)val;
+				StreetSchedule strS;
+				strS.streetName = streets[i];
+				strS.greenDuration = lightDuration[i];
+				s.schedules.push_back(strS);
+			}
+		}
+
+		out.schedules.push_back(s);
+	}
+
+	out.numSchedules = out.schedules.size();
+
+	//std::cout << input.toString();
 
     std::cout << FileManager::writeOutput(out, "./outputs/" + fileName + ".out") << std::endl;
 }
@@ -17,7 +71,7 @@ void processFile(const std::string& fileName) {
 
 int main()
 {
-	std::vector<std::string> files = { "prova" };
+	std::vector<std::string> files = { "a", "b", "c", "d", "e", "f" };
 	for (auto fName : files) {
 		std::cout << "Begin " << fName << std::endl;
 		processFile(fName);
